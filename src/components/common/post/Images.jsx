1b754eb25a589a715427c87d62ccdbfb0e7055e2
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-12-23 17:19:19 +0530

    removed image slider and made custom one

--- src/components/common/post/Images.jsx
+++ src/components/common/post/Images.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from "react";
-import SimpleImageSlider from "react-simple-image-slider";
-import ModalImage, { Lightbox } from "react-modal-image";
-
+import ModalImage from "react-modal-image";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 function PostImages({ images, circle }) {
-  const [openLightbox, setOpenLightbox] = useState(false);
-  const [currentImage, setCurrentImage] = useState("");
+  const [currentImage, setCurrentImage] = useState(images[0]);
 
-  const closeLightbox = () => {
-    setOpenLightbox(false);
+  const chaangeImage = (direction) => {
+    if (direction === "left") {
+      const currentIndex = images.indexOf(currentImage);
+      const nextIndex = currentIndex === 0 ? 0 : currentIndex - 1;
+      setCurrentImage(images[nextIndex]);
+    }
+    if (direction === "right") {
+      const currentIndex = images.indexOf(currentImage);
+      const nextIndex =
+        currentIndex === images.length - 1 ? currentIndex : currentIndex + 1;
+      setCurrentImage(images[nextIndex]);
+    }
   };
 
-  const onImageClicked = (index, event) => {
-    event.preventDefault();
-    console.log(index);
-    setCurrentImage(images[index]);
-    setOpenLightbox(true);
-  };
   return (
     <>
       <div className='relative mt-4 justify-center rounded-sm overflow-hidden'>
         {images.length > 1 ? (
-          <div className='rounded-3xl simpleImageSlider w-full min-h-[50px] max-h-[300px] md:min-h-[200px] md:max-h-[700px] h-full overflow-hidden'>
-            <SimpleImageSlider
-              width={`100%`}
-              height={`600px`}
-              images={images}
-              showBullets={false}
-              showNavs={true}
+          <div className='rounded-3xl simpleImageSlider flex items-center'>
+            <button
+              className='hover:bg-gray-100 dark:hover:bg-gray-700 py-8'
+              onClick={() => chaangeImage("left")}>
+              <BsChevronLeft size={24} />
+            </button>
+            <div className="w-full">
+            <ModalImage
+              small={currentImage}
+              large={currentImage}
+              hideDownload={true}
+              className='min-h-[50px] !max-h-[300px] md:!max-h-[600px] h-full  rounded-md border-2 border-gray-100 dark:border-gray-800'
               alt={circle.Username}
-              style={{ borderRadius: "1.5rem" }}
-              onClick={(idx, event) => onImageClicked(idx, event)}
             />
+            </div>
+            <button
+              className='hover:bg-gray-100 dark:hover:bg-gray-700 py-8'
+              onClick={() => chaangeImage("right")}>
+              <BsChevronRight size={24} />
+            </button>
           </div>
         ) : (
           images[0] !== "" && (
@@ -39,19 +50,11 @@ function PostImages({ images, circle }) {
                 small={images[0]}
                 large={images[0]}
                 hideDownload={true}
-                className='min-h-[50px] !max-h-[300px] md:!max-h-[600px] h-full'
+                className='min-h-[50px] !max-h-[300px] md:!max-h-[600px] h-full rounded-md border-2 border-gray-100 dark:border-gray-800'
                 alt={circle.Username}
               />
             </div>
           )
-        )}
-        {openLightbox && (
-          <Lightbox
-            medium={currentImage}
-            large={currentImage}
-            alt='CircleIt'
-            onClose={closeLightbox}
-          />
         )}
       </div>
     </>
